4c08ce321bf1fd6d5db46f2773fbedfcdc7c45b4
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-09-01 14:59:29 +0900

    Add freshly source code

---
+++ app/src/main/java/com/example/kakakikikeke/sensortest/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.kakakikikeke.sensortest;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "SensorTest";
+    private SensorManager mSensorManager;
+    private TextView[] mSensor = new TextView[3];
+    private float[] answers = new float[3];
+    private float[] mGeomagnetic;
+    private float[] mAcceleration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor[0] = (TextView) findViewById(R.id.sensor_0_text);
+        mSensor[1] = (TextView) findViewById(R.id.sensor_1_text);
+        mSensor[2] = (TextView) findViewById(R.id.sensor_2_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] inR = new float[16];
+        float[] outR = new float[16];
+        float[] I = new float[16];
+        float[] mOrientation = new float[3];
+        Log.i(TAG, "Strart onSensorChanged");
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+            Log.i(TAG, "SENSOR_STATUS_UNRELIABLE");
+            return;
+        }
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mGeomagnetic = event.values.clone();
+                Log.i(TAG, "Get the value of magnetic field");
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                Log.i(TAG, "Get the value of accelerometer");
+                mAcceleration = event.values.clone();
+                break;
+        }
+        if (mGeomagnetic != null && mAcceleration != null) {
+            SensorManager.getRotationMatrix(inR, I, mGeomagnetic, mAcceleration);
+            SensorManager.remapCoordinateSystem(inR, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
+            SensorManager.getOrientation(outR, mOrientation);
+            Log.i(TAG, "orientation[Z] : " + radianToDegree(mOrientation[0]));
+            Log.i(TAG, "orientation[X] : " + radianToDegree(mOrientation[1]));
+            Log.i(TAG, "orientation[Y] : " + radianToDegree(mOrientation[2]));
+            mSensor[0].setText(String.valueOf("Z : " + radianToDegree(mOrientation[0])));
+            mSensor[1].setText(String.valueOf("X : " + radianToDegree(mOrientation[1])));
+            mSensor[2].setText(String.valueOf("Y :" + radianToDegree(mOrientation[2])));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensors) {
+            if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+            }
+            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    private void initAnswers() {
+        for (float i : answers) {
+            double v = Math.random() * 180;
+        }
+    }
+
+    int radianToDegree(float rad) {
+        return (int) Math.floor(Math.toDegrees(rad));
+    }
+}
