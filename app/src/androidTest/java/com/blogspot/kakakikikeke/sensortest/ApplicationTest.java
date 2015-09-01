693055daa8620e59ab70926f1050194bd7c33eb6
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-09-01 18:01:02 +0900

    Move to package name com.blogspot.kakakikikeke from com.example.kakakikikeke

--- app/src/androidTest/java/com/blogspot/kakakikikeke/sensortest/ApplicationTest.java
+++ app/src/androidTest/java/com/blogspot/kakakikikeke/sensortest/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.kakakikikeke.sensortest;
+package com.blogspot.kakakikikeke.sensortest;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/blogspot/kakakikikeke/sensortest/MainActivity.java
+++ app/src/main/java/com/blogspot/kakakikikeke/sensortest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.kakakikikeke.sensortest;
+package com.blogspot.kakakikikeke.sensortest;
 
 import android.content.Context;
 import android.hardware.Sensor;
