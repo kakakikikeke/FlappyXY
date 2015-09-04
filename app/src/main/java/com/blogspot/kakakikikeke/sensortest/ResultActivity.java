e0a4f51068b76e033eb4e8b3933c2154b52fe92f
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-09-04 18:08:07 +0900

    Enable to show cleared count in ResultActivity

--- app/src/main/java/com/blogspot/kakakikikeke/sensortest/ResultActivity.java
+++ app/src/main/java/com/blogspot/kakakikikeke/sensortest/ResultActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.blogspot.kakakikikeke.sensortest.utils.Const;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView clearCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
+
+        Intent i = getIntent();
+        clearCount = (TextView) findViewById(R.id.clear_count);
+        String label = (String) clearCount.getText();
+        clearCount.setText(label + i.getIntExtra(Const.CLEAR_COUNT, 0));
     }
 
     public void reStartGame(View view) {
