939afdf03a6594c2cc8ecd7d3a7cfc288131124e
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-09-10 17:13:21 +0900

    Enable to regist a deviceToken

--- app/src/main/java/com/blogspot/kakakikikeke/sensortest/StartActivity.java
+++ app/src/main/java/com/blogspot/kakakikikeke/sensortest/StartActivity.java
@@ -2,16 +2,29 @@ package com.blogspot.kakakikikeke.sensortest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 
+import com.blogspot.kakakikikeke.sensortest.utils.ConfigPropUtil;
+import com.nifty.cloud.mb.NCMB;
+import com.nifty.cloud.mb.NCMBException;
+import com.nifty.cloud.mb.NCMBInstallation;
+import com.nifty.cloud.mb.NCMBPush;
+import com.nifty.cloud.mb.RegistrationCallback;
+
 public class StartActivity extends AppCompatActivity {
+
+    private ConfigPropUtil config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+        config = new ConfigPropUtil();
+        initNCMB();
+        registDeviceTokenToNCMB();
     }
 
     public void startGame(View view) {
@@ -25,10 +38,31 @@ public class StartActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean dispatchKeyEvent(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-            return true;
-        }
-        return super.dispatchKeyEvent(e);
+    public boolean dispatchKeyEvent(@NonNull KeyEvent e) {
+        return e.getKeyCode() == KeyEvent.KEYCODE_BACK || super.dispatchKeyEvent(e);
+    }
+
+    private void initNCMB() {
+        NCMB.initialize(this, (String) config.get("ncmb.apikey"), (String) config.get("ncmb.clientkey"));
+    }
+
+    private void registDeviceTokenToNCMB() {
+        final NCMBInstallation installation = NCMBInstallation.getCurrentInstallation();
+        installation.getRegistrationIdInBackground((String) config.get("gcm.senderId"), new RegistrationCallback() {
+            @Override
+            public void done(NCMBException e) {
+                if (e != null) {
+                    // fail
+                } else {
+                    // success
+                    try {
+                        installation.save();
+                    } catch (NCMBException e2) {
+                        e2.printStackTrace();
+                    }
+                }
+            }
+        });
+        NCMBPush.setDefaultPushCallback(this, MainActivity.class);
     }
 }
