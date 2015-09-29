5648c1ab88338f2b9368a90d01529c2f9dc7bb61
kakakikikeke <cdl.yoshinaka@gmail.com>
2015-09-29 21:03:30 +0900

    Able to regist player's name and player's score

--- app/src/main/java/com/blogspot/kakakikikeke/sensortest/ResultActivity.java
+++ app/src/main/java/com/blogspot/kakakikikeke/sensortest/ResultActivity.java
@@ -3,16 +3,25 @@ package com.blogspot.kakakikikeke.sensortest;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.blogspot.kakakikikeke.sensortest.utils.Const;
+import com.nifty.cloud.mb.NCMBException;
+import com.nifty.cloud.mb.NCMBObject;
+import com.nifty.cloud.mb.SaveCallback;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private static final String RANKING_CLASS = "ranking";
+    private static final String NAME_FIELD = "name";
+    private static final String SCORE_FIELD = "score";
     private TextView clearCount;
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +29,12 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.result_activity);
 
         Intent i = getIntent();
+        TextView clearCountLabel = (TextView) findViewById(R.id.clear_count_label);
+        clearCountLabel.setTypeface(Typeface.createFromAsset(getAssets(), Const.FONT_NAME));
         clearCount = (TextView) findViewById(R.id.clear_count);
         clearCount.setTypeface(Typeface.createFromAsset(getAssets(), Const.FONT_NAME));
-        String label = (String) clearCount.getText();
-        clearCount.setText(label + i.getIntExtra(Const.INTENT_INDEX_NAME_CLEAR_COUNT, 0));
+        clearCount.setText(String.valueOf(i.getIntExtra(Const.INTENT_INDEX_NAME_CLEAR_COUNT, 0)));
+        userName = (EditText) findViewById(R.id.name);
     }
 
     public void reStartGame(View view) {
@@ -37,10 +48,29 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean dispatchKeyEvent(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-            return true;
-        }
-        return super.dispatchKeyEvent(e);
+    public boolean dispatchKeyEvent(@NonNull KeyEvent e) {
+        return e.getKeyCode() == KeyEvent.KEYCODE_BACK || super.dispatchKeyEvent(e);
+    }
+
+    public void registUser(View view) {
+        String name = userName.getText().toString();
+        String score = clearCount.getText().toString();
+        registUserToNCMB(name, Integer.parseInt(score));
+    }
+
+    private void registUserToNCMB(String name, int score) {
+        NCMBObject obj = new NCMBObject(RANKING_CLASS);
+        obj.put(NAME_FIELD, name);
+        obj.put(SCORE_FIELD, score);
+        obj.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(NCMBException e) {
+                if(e != null){
+
+                }else {
+
+                }
+            }
+        });
     }
 }
